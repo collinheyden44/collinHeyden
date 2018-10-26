c2bf3bcfc2336e62fe5d551fbd4e9fe58f24bc38
Collin Heyden <collinheyden44@gmail.com>
2018-10-26 01:56:52 -0500

    Activ4Test

---
+++ collinHeyden/src/oOP/Activ4Test.java
@@ -0,0 +1,16 @@
+package oOP;
+
+public class Activ4Test {
+
+	public static void main(String[] args) {
+		Friend jenny = new Friend();
+		Friend billy = new Friend();
+		Friend sammy = new Friend();
+		Friend kyle = new Friend();
+		billy.sayMessage();
+		kyle.sayMessage();
+
+
+	}
+
+}
