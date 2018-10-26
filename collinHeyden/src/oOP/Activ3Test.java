1abfb4dca13bcbae583d37cdc282ae1290412089
Collin Heyden <collinheyden44@gmail.com>
2018-10-26 01:56:41 -0500

    Activ3Test

---
+++ collinHeyden/src/oOP/Activ3Test.java
@@ -0,0 +1,16 @@
+package oOP;
+
+public class Activ3Test {
+
+	public static void main(String[] args) {
+		Greeter bob = new Greeter();
+		Friend john = new Friend();
+		bob.sayHello();
+		bob.sayHello();
+		bob.sayHello();
+		john.sayMessage();
+		bob.sayHello();
+
+	}
+
+}
