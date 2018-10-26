a6d96c11768cb8b5aff8fdc78a0d6ffe80c6ac33
Collin Heyden <collinheyden44@gmail.com>
2018-10-26 01:58:00 -0500

    NumberTest

---
+++ collinHeyden/src/oOP/NumberTest.java
@@ -0,0 +1,13 @@
+package oOP;
+
+public class NumberTest {
+
+	public static void main(String[] args) {
+		
+		Number bob = new Number();
+		bob.sayNumber(3);
+		bob.returnRoundUp(4.9);
+		
+	}
+
+}
