838fe5578bea44a73421c58585ff51e3f9c7c135
Collin Heyden <collinheyden44@gmail.com>
2018-10-30 11:27:34 -0500

    Promotion

---
+++ collinHeyden/src/decisionStructures/Promotion.java
@@ -0,0 +1,36 @@
+package decisionStructures;
+
+import java.util.Scanner;
+
+public class Promotion {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double close = .1;
+		boolean promotion = true;
+		System.out.println("How old are you?");
+		int age = input.nextInt();
+		System.out.println("How many years of experience do you have?");
+		double experience = input.nextDouble();
+		System.out.println("What is your height (in inches)?");
+		int height = input.nextInt();
+		
+		if(age >= 50) {
+			if(experience - Math.abs(10.5) < close) {
+				if (height > 80 || height < 70) {
+					promotion = true;
+				}
+			}
+		}
+		else {
+			if(height > 70 && height < 80) {
+			promotion = false;
+			}
+		}
+		
+		System.out.println("YYou get a promotion: " + promotion);
+
+
+	}
+
+}
