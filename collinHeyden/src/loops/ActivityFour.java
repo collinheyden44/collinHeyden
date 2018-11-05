cea9f65c4a5561e166ae341d882da88adc65cb13
Collin Heyden <collinheyden44@gmail.com>
2018-11-05 09:54:22 -0600

    ActivityFourUpdated

--- collinHeyden/src/loops/ActivityFour.java
+++ collinHeyden/src/loops/ActivityFour.java
@@ -6,17 +6,17 @@ public class ActivityFour {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("What number do you want to find the positive multiples of 2 for?");
+		System.out.println("What number do you want to find the sum of the positive multiples of 2 for?");
 		int sum = 0;
 		int num = input.nextInt();
 		int count = 0;
-		System.out.println("List of Positive multiples of 2 for " + num + "\n");
+		
 		
 		while(count <= num) {
 			sum += count;
 			count += 2;
 		}
-		
+		System.out.println("\n" + "Sum of positive multiples of 2 for " + num);
 		System.out.println(sum);
 		input.close();
 
