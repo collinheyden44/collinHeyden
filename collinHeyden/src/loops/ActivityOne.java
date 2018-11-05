432b4262da0b2ddb04c4033756068602fca75eb5
Collin Heyden <collinheyden44@gmail.com>
2018-11-05 08:51:17 -0600

    ActivityOne

---
+++ collinHeyden/src/loops/ActivityOne.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class ActivityOne {
+
+	public static void main(String[] args) {
+		int count = 0;
+		System.out.println("List of Numbers 0-100" + "\n");
+		
+		while(count < 101) {
+			System.out.println(count);
+			count ++;
+		}
+
+	}
+
+}
