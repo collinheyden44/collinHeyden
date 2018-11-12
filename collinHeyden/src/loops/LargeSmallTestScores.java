106238247dd41277f64d308a3a8d0e93332ad7f7
Collin Heyden <collinheyden44@gmail.com>
2018-11-12 10:07:03 -0600

    LargeSmallTestScoresUpdated

--- collinHeyden/src/loops/LargeSmallTestScores.java
+++ collinHeyden/src/loops/LargeSmallTestScores.java
@@ -6,19 +6,25 @@ public class LargeSmallTestScores {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		double test = 0.0;
-		double testHigh = 100.0;
-		double testLow = 0.0;
-		while(test != 9999) {
-			System.out.println("What is your test score? (ex. 0.97)");
+		double test = 0;
+		double testHigh = 0.0;
+		double testLow = 100.0;
+		while(test != 9999 && test >= 0) {
+			System.out.println("What is your test score? (ex. 0.97) - 9999 to quit");
 			test = input.nextDouble();
 			
-			if(test)
+			if(test > testHigh && test != 9999) {
+				testHigh = test;
+			}
+			
+			if(test < testLow) {
+				testLow = test;
+			}
 		}
 		
-		System.out.println("Your Highest Test Score was: " + testHigh);
+		System.out.println("Your Highest Test Score Was: " + testHigh);
 		System.out.println("Your Lowest Test Score Was: " + testLow);
-
+		input.close();
 	}
 
 }
