dcddab4cd51c78994c0b993c2663a719a6cb758d
Collin Heyden <collinheyden44@gmail.com>
2018-11-12 08:37:39 -0600

    Activ1

---
+++ collinHeyden/src/loops/Activ1.java
@@ -0,0 +1,22 @@
+package loops;
+
+import java.util.Scanner;
+
+public class Activ1 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter a number");
+		int num = input.nextInt();
+		
+		do {
+			System.out.println("Magic Number: " + (num * 17));
+			System.out.println("Please enter a Number.class (9999 to quit)");
+			num = input.nextInt();
+		}
+		while(num != 9999);
+
+	}
+
+}
