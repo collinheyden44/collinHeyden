f159ba5027319257ec2a92ce94156f23efb9277a
Collin Heyden <collinheyden44@gmail.com>
2018-11-15 09:27:06 -0600

    Activity0Nested

---
+++ collinHeyden/src/loops/Activity0Nested.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class Activity0Nested {
+
+	public static void main(String[] args) {
+		
+		for(int counter1 = 1; counter1 <= 5; counter1++) {
+			for(int counter2 = 1; counter2 <= 10; counter2++) {
+				System.out.println(counter1 + " Hello, World!");
+			}
+		}
+
+	}
+
+}
