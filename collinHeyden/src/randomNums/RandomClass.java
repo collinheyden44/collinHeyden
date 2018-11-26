74eecadd9512f99c263d538d4d71513dd6af7f3d
Collin Heyden <collinheyden44@gmail.com>
2018-11-26 09:04:33 -0600

    RandomClassUpdated

--- collinHeyden/src/randomNums/RandomClass.java
+++ collinHeyden/src/randomNums/RandomClass.java
@@ -1,9 +1,19 @@
 package randomNums;
 
+import java.util.Random;
+
 public class RandomClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Random generator = new Random();
+		
+		int min = 0;
+		int max = 100;
+		
+		int randomNum = min + generator.nextInt(max -  min + 1);
+		
+		System.out.println("A random number between " + min + " and " + max + " is: " + randomNum);
 
 	}
 
