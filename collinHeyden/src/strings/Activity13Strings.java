638f35f6b1ebe90ff645c2b5b47b50ca7a2a709a
Collin Heyden <collinheyden44@gmail.com>
2018-11-30 08:59:45 -0600

    Activity13Strings

---
+++ collinHeyden/src/strings/Activity13Strings.java
@@ -0,0 +1,19 @@
+package strings;
+
+import java.util.Scanner;
+
+public class Activity13Strings {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please tyoe in your social security number (example: 123-45-6789)");
+		String sSN = input.nextLine();
+
+		System.out.println(sSN.substring(7, 10));
+		System.out.println(sSN.substring(4, 6));
+		System.out.println(sSN.substring(0, 3));
+			
+	}
+
+}
