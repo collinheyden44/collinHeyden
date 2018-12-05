d02854ad728b9de8cdf9bee555b035dd17373a28
Collin Heyden <collinheyden44@gmail.com>
2018-12-05 11:20:39 -0600

    PasswordTest

---
+++ collinHeyden/src/strings/PasswordTest.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class PasswordTest {
+
+	public static void main(String[] args) {
+		Password bob = new Password();
+		bob.setPassword();
+		System.out.println(bob.setPassword());
+		bob.match();
+
+	}
+
+}
