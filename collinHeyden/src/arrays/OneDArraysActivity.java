f58e631becc2dffb1136ca92a625fbba8f2515a0
Collin Heyden <collinheyden44@gmail.com>
2018-12-07 08:42:27 -0600

    OneDArraysActivityUnfinished

---
+++ collinHeyden/src/arrays/OneDArraysActivity.java
@@ -0,0 +1,19 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class OneDArraysActivity {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		final int maxStudents = 2500;
+		double[] scores = new double[maxStudents];
+		
+		for(int i = 0; i < maxStudents; i++) {
+			System.out.println("Please type in your students");
+			
+		}
+
+	}
+
+}
