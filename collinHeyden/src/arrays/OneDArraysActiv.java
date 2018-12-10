a42725bf16340821e70874170cca533528f65cb7
Collin Heyden <collinheyden44@gmail.com>
2018-12-10 11:10:37 -0600

    OneDArraysActiv

---
+++ collinHeyden/src/arrays/OneDArraysActiv.java
@@ -0,0 +1,24 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class OneDArraysActiv {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		int numNames = 20;
+		String[] names = new String[numNames];
+		
+		for(int i = 0; i < numNames; i++) {
+			System.out.println("Type in name below:");
+			names[i] = input.nextLine();
+		}
+		
+		for(int j = 20; j >= 0; j--) {
+			System.out.println(names[j]);
+		}
+		input.close();
+	}
+
+}
