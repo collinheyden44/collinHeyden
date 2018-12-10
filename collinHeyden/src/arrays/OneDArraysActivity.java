eab6633d32fa31e633885f1853258b67bbdb926b
Collin Heyden <collinheyden44@gmail.com>
2018-12-10 11:11:11 -0600

    OneDArraysActivity

--- collinHeyden/src/arrays/OneDArraysActivity.java
+++ collinHeyden/src/arrays/OneDArraysActivity.java
@@ -6,14 +6,18 @@ public class OneDArraysActivity {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		final int maxStudents = 2500;
+		final int maxStudents = 10;
 		double[] scores = new double[maxStudents];
 		
 		for(int i = 0; i < maxStudents; i++) {
 			System.out.println("Please type in your students");
-			
+			scores[i] = input.nextDouble();
 		}
-
+	
+		for(int j = 0; j < maxStudents; j++) {
+			System.out.println(scores[j]);
+			}
+		input.close();
 	}
 
 }
