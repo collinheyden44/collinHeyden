119d75eab0b13dc79c2037972102130ad7dcdf8f
Collin Heyden <collinheyden44@gmail.com>
2018-12-13 02:50:23 -0600

    OneDArraysActiv3

--- collinHeyden/src/arrays/OneDArraysActiv3.java
+++ collinHeyden/src/arrays/OneDArraysActiv3.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class OneDArraysActiv3 {
 
 	public static void main(String[] args) {
+		
 		Scanner input = new Scanner(System.in);
+		
 		final int maxStudents = 10;
 		String[] scores = new String[maxStudents];
 		
@@ -19,6 +21,7 @@ public class OneDArraysActiv3 {
 		int check = input.nextInt();
 		
 		System.out.println("\n" + "Names that are " + check + " letters long:");
+		
 		for(int j = 0; j < maxStudents; j++) {
 			if(check == scores[j].length()) {
 				System.out.println(scores[j]);
