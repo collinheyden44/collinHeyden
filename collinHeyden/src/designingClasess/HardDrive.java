14df3fc1035396d602e27ac2917cf3fbb28cbd57
Collin Heyden <collinheyden44@gmail.com>
2019-01-10 01:29:30 -0600

    HardDrive

--- collinHeyden/src/designingClasess/HardDrive.java
+++ collinHeyden/src/designingClasess/HardDrive.java
@@ -1,10 +1,19 @@
 package designingClasess;
 
+import java.util.ArrayList;
+
 public class HardDrive {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String name = " ";
+	private ArrayList files = new ArrayList(); 
+	private double totalCapacity = 0;
+	private double freeSpace = 0;
+	
+	public HardDrive(String newName, double newTotalCapacity, double newFreeSpace) {
+		name = newName;
+		
+		totalCapacity = newTotalCapacity;
+		freeSpace = newFreeSpace;
 	}
 
 }
